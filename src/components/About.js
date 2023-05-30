983cf0134df9ac952912a6f176714e2a876f6966
sarthaklambaa <samlamba29@gmail.com>
2023-05-30 21:08:49 +0530

    fixes

--- src/components/About.js
+++ src/components/About.js
@@ -36,36 +36,36 @@ export default function About(props) {
                 <div className="accordion-item"  style={{color: props.mode==='dark'?'white':'black', backgroundColor: props.mode==='dark'?'black':'white'}}>
                     <h2 className="accordion-header">
                         <button className="accordion-button"  style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'black'}} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Accordion Item #1
+                            <strong>Analyze your text</strong>
                         </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample" >
                         <div className="accordion-body"  >
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        Our state-of-the-art text analysis tools provide you with invaluable insights, allowing you to understand your writing on a whole new level. By utilizing our advanced algorithms and linguistic analysis, you can uncover hidden patterns, detect redundancies, and fine-tune your content to perfection. From assessing readability and word count to ensuring grammatical accuracy and exploring vibrant vocabulary, our text analysis capabilities are unparalleled. Gain a comprehensive understanding of your text and elevate your writing to new heights with our powerful suite of analytical tools.
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item" style={{color: props.mode==='dark'?'white':'black', backgroundColor: props.mode==='dark'?'black':'white'}}>
                     <h2 className="accordion-header">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'black'}}>
-                            Accordion Item #2
+                           <strong>Free to use</strong>
                         </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                         <div className="accordion-body"  >
-                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        We firmly believe that text analysis tools should be accessible to all. That's why we're proud to offer our services completely free of charge. We understand the importance of empowering writers, students, and professionals with the ability to enhance their text without any financial barriers. Our commitment to making text analysis tools accessible means that you can utilize our comprehensive suite of features without any subscription fees or hidden costs. Whether you need to analyze readability, check grammar and spelling, find synonyms, or translate text, our tools are available to you at no cost. We believe that everyone should have the opportunity to improve their writing and create impactful content, regardless of their financial circumstances.
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item" style={{color: props.mode==='dark'?'white':'black', backgroundColor: props.mode==='dark'?'black':'white'}}>
                     <h2 className="accordion-header">
                         <button className="accordion-button collapsed"   type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'black'}}>
-                            Accordion Item #3
+                            <strong>Browser Compatible</strong>
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                         <div className="accordion-body"  >
-                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        This website is designed to be fully compatible with all major web browsers, ensuring a seamless text analysis experience across different platforms. Whether you prefer Chrome, Firefox, Safari, or any other popular browser, you can access our tools without any compatibility issues. Our responsive web design ensures that you can enjoy the same user-friendly interface and robust functionality, regardless of the browser you choose. Whether you're analyzing text on your computer, laptop, tablet, or smartphone, TextUtils.com adapts effortlessly to your device, allowing you to access our powerful text analysis tools on the go. Say goodbye to compatibility concerns and enjoy a consistent and reliable text analysis experience across all your browsing devices.
                         </div>
                     </div>
                 </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,10 +29,7 @@ export default function Textform(props) {
     }
 
     const handleCopy = () =>{
-        var text = document.getElementById("myBox");
-        text.select();
-        text.setSelectionRange(0,999);
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert("Successfully copied the text!", "success");
     }
 
@@ -56,19 +53,19 @@ export default function Textform(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'black' }} id="myBox" rows="15"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpper}>Convert to Uppercase</button>
-            <button className="btn btn-danger mx-1 my-1" onClick={handleLower}>Convert to LowerCase</button>
-            <button className="btn btn-warning mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary" onClick={handleUpper}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={handleLower}>Convert to LowerCase</button>
+            <button disabled={text.length===0} className="btn btn-warning mx-1 my-1" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-success mx-1 my-1" onClick={handlePaste}>Paste Text</button>
-            <button className="btn btn-info mx-1 my-1" onClick={handleClear}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-info mx-1 my-1" onClick={handleClear}>Clear Text</button>
         </div>
 
         <div className="container my-4" style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>It will take about <b>{0.008 * text.split(" ").length} minutes </b> to read the above text</p>
+            <p>{text.split(/\s+/).filter((element) =>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>It will take about <b>{0.008 * text.split(" ").filter((element) =>{return element.length!==0}).length} minutes </b> to read the above text</p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:"Enter something to preview it here"}</p>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
         </div>
         </>
     )
